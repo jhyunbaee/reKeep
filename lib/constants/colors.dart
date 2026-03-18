7037f1bd9d5a00ac171cfd30c2fc7dcccc9406a6
jhyunbaee <jhyunbaee@gmail.com>
2026-03-18 15:16:03 +0900

    feat: asset 설정

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const primary = Color(0xFF4F7CFF);
-  static const secondary = Color(0xFF00C853);
+  static const secondary = Color(0xFF777C89);
   static const textPrimary = Color(0xFF000000);
   static const background = Color(0xFFFFFFFF);
   static const fieldColor = Color(0xFFF2F3F7);
+  static const pointColor = Color(0xFFFF6B6B);
 }
