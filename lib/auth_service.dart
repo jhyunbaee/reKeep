986962b9fe6d504b4fa5620460508998facc07be
jhyunbaee <jhyunbaee@gmail.com>
2026-03-23 15:18:05 +0900

    feat: Auth Service 구현

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -13,6 +13,7 @@ class AuthService extends ChangeNotifier {
     required String email,
     required String password,
     required String name,
+    required String nickname,
     required String phone,
     required Function() onSuccess,
     required Function(String err) onError,
@@ -26,16 +27,15 @@ class AuthService extends ChangeNotifier {
     }
 
     try {
-      /// Firebase Auth 회원가입
       final credential = await FirebaseAuth.instance
           .createUserWithEmailAndPassword(email: email, password: password);
 
       final user = credential.user;
 
-      /// Firestore 사용자 정보 저장
       await FirebaseFirestore.instance.collection("users").doc(user!.uid).set({
         "email": email,
         "name": name,
+        "nickname": nickname,
         "phone": phone,
         "createdAt": Timestamp.now(),
       });
@@ -79,6 +79,67 @@ class AuthService extends ChangeNotifier {
 
   Future<void> signOut() async {
     await FirebaseAuth.instance.signOut();
-    notifyListeners(); // ⭐ 필수
+    notifyListeners();
   }
+
+  Future<void> deleteAccount() async {
+    final user = FirebaseAuth.instance.currentUser;
+
+    if (user == null) return;
+
+    try {
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .delete();
+      await user.delete();
+      notifyListeners();
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'requires-recent-login') {
+        throw Exception("최근 로그인 필요");
+      } else {
+        throw Exception(e.message);
+      }
+    }
+  }
+
+  // 🔹 비밀번호 변경 메서드 추가
+  Future<void> updatePassword({
+    required String currentPassword,
+    required String newPassword,
+  }) async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) throw Exception("로그인된 유저가 없습니다.");
+
+    try {
+      // 🔑 현재 비밀번호로 재인증
+      final cred = EmailAuthProvider.credential(
+        email: user.email!,
+        password: currentPassword,
+      );
+      await user.reauthenticateWithCredential(cred);
+
+      // 🔑 새 비밀번호로 업데이트
+      await user.updatePassword(newPassword);
+
+      notifyListeners();
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'wrong-password') {
+        throw Exception("현재 비밀번호가 올바르지 않습니다.");
+      } else if (e.code == 'requires-recent-login') {
+        throw Exception("최근 로그인 필요");
+      } else {
+        throw Exception(e.message);
+      }
+    }
+  }
+}
+
+// 닉네임 중복 확인
+Future<bool> checkNicknameDuplicate(String nickname) async {
+  final query = await FirebaseFirestore.instance
+      .collection("users")
+      .where("nickname", isEqualTo: nickname)
+      .get();
+  return query.docs.isNotEmpty;
 }
